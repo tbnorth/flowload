421fd6b4cafa453ff30eb71ccda95298705b3574
terrynbrown@gmail.com <terrynbrown@gmail.com>
2018-07-09 13:31:55 -0500

    plot flow vs. load

--- flowload.py
+++ flowload.py
@@ -5,7 +5,15 @@ from scipy import stats
 
 lds = pd.read_csv("Grand_MuskegonHistoricalLoadings.csv", sep='[, ]')
 
-plt.scatter(lds.Year, lds.TP_Grand_MT)
+lds.set_index('Year', inplace=True)
+yrs = pd.read_csv("grand_flow.csv")
+yrs.set_index('year', inplace=True)
+
+lds = pd.concat([lds, yrs], axis=1, join='inner')
+
+lds.apply(lambda x: plt.text(x.flow, x.TP_Grand_MT, x.name), axis=1)
+
+plt.scatter(lds.flow, lds.TP_Grand_MT)
 plt.show()
 
 
