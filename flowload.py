219519c598fbb372c9b6924cda96af2b72af65af
terrynbrown@gmail.com <terrynbrown@gmail.com>
2018-07-09 13:42:02 -0500

    show unknown years

--- flowload.py
+++ flowload.py
@@ -14,6 +14,27 @@ lds = pd.concat([lds, yrs], axis=1, join='inner')
 lds.apply(lambda x: plt.text(x.flow, x.TP_Grand_MT, x.name), axis=1)
 
 plt.scatter(lds.flow, lds.TP_Grand_MT)
+
+slope, intercept, r_value, p_value, std_err = stats.linregress(lds.flow, lds.TP_Grand_MT)
+
+site = '04119000'
+lds.sort_values('flow', inplace=True)
+
+plt.title("Site: %s r2: %s p: %s" % (site, round(r_value**2, 2), round(p_value, 2)))
+plt.xlabel("Mean daily cfs")
+plt.ylabel("Total P MT (annual load)")
+plt.scatter(lds['flow'], lds['TP_Grand_MT'])
+plt.plot(
+    [lds['flow'].values[0], lds['flow'].values[len(lds)-1]],
+    [lds['flow'].values[0]*slope+intercept, lds['flow'].values[len(lds)-1]*slope+intercept],
+)
+
+for i in range(2010, 2016):
+    flow = yrs.loc[i]['flow']
+    load = intercept+slope*flow
+    plt.scatter(flow, load, c='C1')
+    plt.text(flow, load, '%s  '%i, horizontalalignment='right')
+
 plt.show()
 
 
